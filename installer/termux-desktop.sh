45e270b2041bf0c59e165cb271e31a3fe3ac33e9
Marx <89106931+Marxde5@users.noreply.github.com>
2022-05-27 20:21:26 +0700

    Update and rename termux-xfce-desktop.sh to termux-desktop.sh

--- installer/termux-desktop.sh
+++ installer/termux-desktop.sh
@@ -23,13 +23,9 @@ chmod +x .vnc/xstartup
 
 wget -q $link/xfce/vncstart -P $PREFIX/bin
 wget -q $link/xfce/vncstop -P $PREFIX/bin
+wget -q $link/xfce/vnc -P $PREFIX/bin
 chmod +x $PREFIX/bin/vncstart
 chmod +x $PREFIX/bin/vncstop
-
-echo "#!/data/data/com.termux/files/usr/bin/bash
-vncserver -localhost  $GEO :$PORT
-echo "VNC server started. The default password is the account password for $USER" >> $PREFIX/bin/vnc
-
 chmod +x $PREFIX/bin/vnc
 
 echo "Anda sekarang dapat memulai vncserver dengan menjalankan tstart"
@@ -42,12 +38,9 @@ echo "Anda dapat terhubung ke alamat ini dengan VNC Viewer yang Anda inginkan"
 echo " "
 echo "Hubungkan ke alamat ini akan membuka jendela dengan Lingkungan Desktop Xfce4"
 echo " "
-echo " "
 echo "Menjalankan vncstart"
 echo " "
-echo " "
-echo "Untuk Membunuh Server VNC jalankan tstop"
-echo " "
+echo "Untuk Membunuh Server VNC jalankan vncstop"
 echo " "
 
 vncpasswd
